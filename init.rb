fe111cfc69b1274ddf03dad350eb730abdc00c5a
Kai <garnetrook@gmail.com>
2010-10-19 08:53:19 +0000

    Project menu tab added

--- init.rb
+++ init.rb
@@ -2,9 +2,16 @@ require 'redmine'
 
 Redmine::Plugin.register :redmine_contacts do
   name 'Redmine Contacts plugin'
-  author 'Author name'
+  author 'Kai Chang'
   description 'This is a plugin for Redmine'
   version '0.0.1'
   url 'http://example.com/path/to/plugin'
   author_url 'http://example.com/about'
+
+  project_module :redmine_contacts do
+    permission :view_contacts, {:contacts => [:index]}
+    permission :edit_contact, {:contacts => [:edit, :update, :new, :create, :destroy]}
+  end
+
+  menu :project_menu, :contacts, {:controller => 'contacts', :action => 'index'}, :caption => 'Contacts', :after => :activity, :param => :project_id
 end
